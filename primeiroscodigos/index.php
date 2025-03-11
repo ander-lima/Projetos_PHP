43564edf01ceb401b0d4290f45fbebff5393c1b9
Ander <33981822+ander-lima@users.noreply.github.com>
2025-03-11 07:14:39 -0300

    Primeiro commit de Projeto PHP - Terminal

---
+++ primeiroscodigos/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hello Ander \u{1F603}";
+        ?>
+    </h1>
+    <p>Meu primeiro código!!</p>
+</body>
+</html>
