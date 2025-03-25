596d340a1b79b11413b515c7c7eb7373eb6ca5c2
Ander <33981822+ander-lima@users.noreply.github.com>
2025-03-25 18:20:07 -0300

    Desafio 002 (Completo)

--- Desafios/Desafio 002/aleatorizador.php
+++ Desafios/Desafio 002/aleatorizador.php
@@ -7,20 +7,31 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <!-- Versão Ander
     <h1>Gerador de Números Alatórios</h1>
     <p>Gerando número entre 0 e 100...</p>
 
     <?php 
     
-            $numero = mt_rand(0,100);
-            echo "<p>O valor gerado foi: <strong>$numero</strong></p>";
+            //$numero = mt_rand(0,100);
+            //echo "<p>O valor gerado foi: <strong>$numero</strong></p>";
             
     ?>
 
     <form method="post">
         <button type="submit">Gerar outro</button>
     </form>
+    -->
+
+    <!-- Versão Professor -->
+    <main>
+        <h1>Trabalhando com números aleatórios</h1>
+        <?php 
+        $numero = mt_rand(0, 100);
+        echo "<p> Gerando números aleatórios entre 0 e 100... <br> O valor gerado foi <strong>$numero</strong> </br></p>";
+        ?>
+        <button onclick="javascript:document.location.reload()">&#x1F504; Gerar outro </button>
+    </main>
 
 </body>
 </html>
