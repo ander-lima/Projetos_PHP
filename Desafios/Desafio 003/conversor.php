accbcf28d86c057943dc653608cb4380bc68104c
Ander <33981822+ander-lima@users.noreply.github.com>
2025-05-14 19:49:43 -0300

    Desafio 003 - Conclusão

--- Desafios/Desafio 003/conversor.php
+++ Desafios/Desafio 003/conversor.php
@@ -13,24 +13,33 @@
     <main>
         <!-- Versão Ander
         <?php 
+        
             $preco = $_GET["preco"];
             $preco = str_replace(",", ".", $preco); // Substituindo virgula por ponto
             $preco = (float) $preco; // Converter o valor de 'preco' para float
             $convertido = $preco / 5.74;
-            echo "Seus R$" . number_format($preco, 2) . " valem US$" . number_format($convertido, 2) . " em dólar";
+
+            echo "Seus R$" . number_format($preco, 2) . " equivalem a US$" . number_format($convertido, 2) . " em dólar";
+            
         ?>
         -->
     
         <?php 
         //Versão Professor
+     
             $preco = $_GET["preco"];
-            $preco = str_replace(",", ".", $preco); // Substituindo virgula por ponto
+            //$preco = str_replace(",", ".", $preco); // Substituindo virgula por ponto
             $preco = (float) $preco; // Converter o valor de 'preco' para float
-            $convertido = $preco / 5.74;
+            $convertido = $preco / 5.00;
 
+            //echo "Seus R\$" . number_format($preco, 2, ",",  ".") . " equivalem a US\$" . number_format($convertido, 2, ",", ".");                
+    
+            
+            //Formatação de moedas com internalização!
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-            echo "Seus R$" . numfmt_format_currency($padrao, $preco, "BRL") . " valem US\$" . numfmt_format_currency($padrao, $preco, "USD") . " em dólar";
+            echo "Seus " . numfmt_format_currency($padrao, $preco, "BRL") . " valem " . numfmt_format_currency($padrao, $preco, "USD") . " em dólar";
+
         ?>
         <button onclick="javascript:window.location.href='index.php'">Voltar</button>
     </main>
--- Desafios/Desafio 003/index.php
+++ Desafios/Desafio 003/index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio 003</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <main>
-        <h1>Conversor de Moedas</h1>
-        <form action="conversor.php" method="get">
-            <label for="preco">Valor R$: </label>
-            <input type="text" id="preco" name="preco" placeholder="0,00" pattern="\d+(\,\d{1,2})?" required>
-            <input type="submit" value="Converter">
-    </main>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Desafio 003</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <main>
+            <h1>Conversor de Moedas</h1>
+            <form action="conversor.php" method="get">
+                <label for="preco">Valor R$: </label>
+                <input type="number" id="preco" name="preco" step = "0.01" placeholder="0,00" pattern="\d+(\,\d{1,2})?" required>
+                <input type="submit" value="Converter">
+            </main>
 </body>
 </html>
