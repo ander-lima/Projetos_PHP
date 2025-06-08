d60b64714c69fa36eec89b560b3cf3c1ef08beb3
Ander <33981822+ander-lima@users.noreply.github.com>
2025-06-08 15:06:29 -0300

    Desafio 004: Conversor de Moedas com Cotação da API do Banco Central - Primeira vez usando API :)

--- Desafios/Desafio 004/conversor.php
+++ Desafios/Desafio 004/conversor.php
@@ -11,29 +11,30 @@
         <h1>Valor em Dolar</h1>
     </header>
     <main>
-        <!-- Versão Ander
-            <?php 
-        
-                $preco = $_GET["preco"];
-                $preco = str_replace(",", ".", $preco); // Substituindo virgula por ponto
-                $preco = (float) $preco; // Converter o valor de 'preco' para float
-                $convertido = $preco / 5.74;
-
-                echo "Seus R$" . number_format($preco, 2) . " equivalem a US$" . number_format($convertido, 2) . " em dólar";
-            
-            ?>
-        -->
     
         <?php 
         //Versão Professor
      
             $preco = $_GET["preco"];
-            //$preco = str_replace(",", ".", $preco); // Substituindo virgula por ponto
+           
             $preco = (float) $preco; // Converter o valor de 'preco' para float
-            $convertido = $preco / 5.00;
 
-            //echo "Seus R\$" . number_format($preco, 2, ",",  ".") . " equivalem a US\$" . number_format($convertido, 2, ",", ".");                
-    
+            //Cotação vinda da API
+                //Guardar período da busca
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
+
+            //REQUIIÇÃO DE API
+            //Guardar dados do site do banco central
+            // URL gerada no site do banco central
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$fim.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+            $dados = json_decode(file_get_contents($url), true);
+            
+            $cotacao = $dados["value"][0]["cotacaoCompra"];
+
+            //Conversão
+            $convertido = $preco / $cotacao;
             
             //Formatação de moedas com internalização!
             $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
